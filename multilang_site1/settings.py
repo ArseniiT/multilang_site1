eea02a3ab9f27eb4d8c17c9954b4b4ad938d3f17
arseniit <arseniit@gmail.com>
2024-06-21 15:45:54 +0200

    Revoke DJANGO_SECRET_KEY

--- multilang_site1/settings.py
+++ multilang_site1/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fy6$mpw9rjp+i0bkx2!15fo@kpc3brw^c=m8b)-8n*yj^o(rep'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
